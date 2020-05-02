afb13e3abb90176e89308fcabc86183cd1c6b956
Gergely Revay <g@aetherlab.net>
2020-05-02 12:45:03 +0200

    14

---
+++ 14/solution.py
@@ -0,0 +1,17 @@
+import random
+import sys
+
+n_inner = 1
+n_total = 1
+
+while True:
+    x = random.random()
+    y = random.random()
+
+    if (x*x + y*y) <=1:
+        n_inner += 1
+    n_total += 1
+
+    pi = 4*(n_inner / n_total)
+    sys.stdout.write('\rPi is: {:.3f} '.format(pi))
+    sys.stdout.flush()
