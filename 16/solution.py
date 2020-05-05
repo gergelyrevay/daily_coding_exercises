10938cebd932155b7eabe311b3031735e7d66971
Gergely Revay <g@aetherlab.net>
2020-05-05 10:43:08 +0200

    updated with circular buffer

--- 16/solution.py
+++ 16/solution.py
@@ -1,25 +1,26 @@
-log = []
-N = 5
+class Log:
+    def __init__(self):
+        self.log = []
+        self.N = 5
+        self.current = 0
 
-def record(order_id):
-    if len(log) >= 5:
-        log.pop(0)
-        log.append(order_id)
-    else:
-        log.append(order_id)
+    def record(self, order_id):
+        if len(self.log) == self.N:
+            self.log[self.current] = order_id
+        else:
+            self.log.append(order_id)
+        self.current = (self.current + 1) % self.N
 
-def get_last(i):
-    return log[-1*i]
+    def get_last(self, i):
+        return self.log[self.current-i]
 
-
-record(1)
-record(2)
-record(3)
-record(4)
-record(5)
-record(6)
-record(7)
-assert (log[0] == 3), 'Error 1'
-assert (log[N-1] == 7), 'Error 2'
-assert (get_last(3) == 5), 'Error 3'
+l = Log()
+l.record(1)
+l.record(2)
+l.record(3)
+l.record(4)
+l.record(5)
+l.record(6)
+l.record(7)
+assert (l.get_last(3) == 5), 'Error 3'
 print('All passed')
